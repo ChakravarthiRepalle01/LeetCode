89af9e4126647fbc483f5b68ff8b92697c029982
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-26 17:57:15 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,39 @@
 class Solution {
     public int climbStairs(int n) {
-        //TimeComplexity - O(n+n);
-        //SpaceComplexity - O(n+n)
+
+        //Tabulation
+        //Time Complexity - O(n)
+        //Space Complexity - O(n)
         int dp[] = new int[n+1];
 
         Arrays.fill(dp , -1);
 
-        return recursiveHelper(0 , n , dp);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2 ; i<=n ; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+
+        //TimeComplexity - O(n+n);
+        //SpaceComplexity - O(n+n)
+        // int dp[] = new int[n+1];
+
+        // Arrays.fill(dp , -1);
+
+        // return recursiveHelper(0 , n , dp);
     }
 
-    public static int recursiveHelper(int i , int n , int dp[]) {
-        if(i == n) return 1;
-        if(i > n) return 0;
+    //Memoization
+    // public static int recursiveHelper(int i , int n , int dp[]) {
+    //     if(i == n) return 1;
+    //     if(i > n) return 0;
 
-        if(dp[i]!=-1) return dp[i];
+    //     if(dp[i]!=-1) return dp[i];
 
-        return dp[i] = recursiveHelper(i+1 , n , dp) + recursiveHelper(i+2 , n , dp);
-    }
+    //     return dp[i] = recursiveHelper(i+1 , n , dp) + recursiveHelper(i+2 , n , dp);
+    // }
 
 }
