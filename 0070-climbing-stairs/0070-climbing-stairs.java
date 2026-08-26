9acd8cb855b6832c69f16a079a5e5191cd4d5b2d
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-26 17:59:14 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,36 @@
 class Solution {
     public int climbStairs(int n) {
 
+        //Space Optimization
+        //Time Complexity - O(n)
+        //Space Complexity - O(1)
+
+        int prev = 1;
+        int secPrev = 1;
+        int curr;
+        for(int i = 2 ; i<=n ; i++) {
+            curr = prev + secPrev;
+            secPrev = prev;
+            prev = curr;
+        }
+
+        return prev;
+
         //Tabulation
         //Time Complexity - O(n)
         //Space Complexity - O(n)
-        int dp[] = new int[n+1];
+        // int dp[] = new int[n+1];
 
-        Arrays.fill(dp , -1);
+        // Arrays.fill(dp , -1);
 
-        dp[0] = 1;
-        dp[1] = 1;
+        // dp[0] = 1;
+        // dp[1] = 1;
 
-        for(int i = 2 ; i<=n ; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+        // for(int i = 2 ; i<=n ; i++) {
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
 
-        return dp[n];
+        // return dp[n];
 
         //TimeComplexity - O(n+n);
         //SpaceComplexity - O(n+n)
