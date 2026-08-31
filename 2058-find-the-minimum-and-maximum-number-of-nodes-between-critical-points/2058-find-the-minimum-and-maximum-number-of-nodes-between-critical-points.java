a4759ce6b2818fa1bf01a6500f7fc1f5fe13a1a3
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-08-31 09:06:18 +0530

    Time: 4 ms (99.87%), Space: 106.3 MB (31.8%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,39 +10,44 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        
-        if(head == null || head.next == null) return new int[]{-1,-1};
-
-        List<Integer> dist = new ArrayList<Integer>();
+        if(head == null || head.next == null) return new int[] {-1 , -1};
 
         ListNode temp = head.next;
 
-        int prevVal = head.val;
-        int totalDist = 1;
-        while(temp.next!=null) {
-            totalDist++;
-            int nextVal = temp.next.val;
-            int currVal = temp.val;
+        int minDist = Integer.MAX_VALUE;
+        int maxDist = 0;
 
-            if((currVal > nextVal && currVal > prevVal) || (currVal < nextVal && currVal < prevVal)) {
-                dist.add(totalDist);
+        int f_index = -1;
+        int l_index = -1;
+        int idx = 1;
+        int prevVal = head.val;
+
+        while(temp.next!=null) {
+            idx++;
+            int currVal = temp.val;
+            int nextVal = temp.next.val;
+
+            if((currVal > prevVal && currVal > nextVal) || (currVal < prevVal && currVal < nextVal)) {
+                if(f_index == -1) {
+                    f_index = idx;
+                }
+                else if(l_index == -1) {
+                    minDist = (idx-f_index);
+                    l_index = idx;
+                }
+                else {
+                    minDist = Math.min(minDist , idx-l_index);
+                    l_index = idx;
+                }
             }
+
             temp = temp.next;
             prevVal = currVal;
         }
 
-        int ans[] = new int[2];
-        int distSize = dist.size();
+        if(f_index == -1 || l_index == -1) return new int[]{-1,-1};
 
-        if(distSize<2) return new int[]{-1,-1};
+        return new int[]{minDist , l_index-f_index};
 
-        ans[1] = dist.get(distSize-1) - dist.get(0);
-        ans[0] = dist.get(1) - dist.get(0);
-
-        for(int i = 1 ; i<distSize ; i++) {
-            ans[0] = Math.min(ans[0] , dist.get(i) - dist.get(i-1));
-        }
-
-        return ans;
     }
 }
