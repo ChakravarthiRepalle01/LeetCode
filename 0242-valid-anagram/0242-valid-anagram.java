bd09ce73f22b8408d8f53a61e0d79769fd051d56
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-09-23 06:57:31 +0530

    Time: 3 ms (93.87%), Space: 44.3 MB (81.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n1 = s.length();
+        int n2 = t.length();
+
+        int arr[] = new int[26];
+
+        for(int i = 0 ; i<n1 ; i++) arr[s.charAt(i)-'a']++;
+
+        for(int i = 0 ; i<n2 ; i++) arr[t.charAt(i)-'a']--;
+
+        for(int i = 0 ; i<26 ; i++) if(arr[i]!=0) return false;
+
+        return true;
+    }
+}
